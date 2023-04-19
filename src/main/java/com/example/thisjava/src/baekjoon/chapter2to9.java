e7af7105e172e9e046fdcc6398e81f665fc514f4
Kimseonbeen <ksb9242@gmail.com>
2023-04-19 21:59:07 +0900

    chapter 2 to 9 end

---
+++ src/main/java/com/example/thisjava/src/baekjoon/chapter2to9.java
@@ -0,0 +1,51 @@
+package com.example.thisjava.src.baekjoon;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class chapter2to9 {
+
+    /**
+     5
+     index 10 13 10 12 15           [0][0] [0][1] [0][2] [0][3] [0][4]
+     index 12 39 30 23 11           [1][0] [1][1] [1][2] [1][3] [1][4]
+     index 11 25 50 53 15           [2][0] [2][1] [2][2] [2][3] [2][4]
+     index 19 27 29 37 27           [3][0] [3][1] [3][2] [3][3] [3][4]
+     index 19 13 30 13 19           [4][0] [4][1] [4][2] [4][3] [4][4]
+     */
+    public static int solution(int n, int[][] arr) {
+
+        int answer = Integer.MIN_VALUE;
+        int sum1, sum2;
+
+        for (int i = 0; i < n; i++) {
+            sum1=sum2=0;
+            for (int j = 0; j < n; j++) {
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
+            }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
+        }
+        for (int i = 0; i < n; i++) {
+            sum1 = sum2 = 0;
+            sum1 += arr[i][i];
+            sum2 += arr[i][n - i - 1];
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int arr[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = kb.nextInt();
+            }
+        }
+        System.out.println(solution(n, arr));
+    }
+}
